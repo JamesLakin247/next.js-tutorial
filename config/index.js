447d06e0ce46853b3efb3ce88e91b3b69f6f218d
James <77265615+JamesLakin247@users.noreply.github.com>
2022-09-08 13:45:17 +1000

    next_public

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-const dev = process.env.NODE_ENV !== 'production'
+const dev = NEXT_PUBLIC_VERCEL_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : `https://${VERCEL_URL}`
+export const server = dev ? 'http://localhost:3000' : `https://${NEXT_PUBLIC_VERCEL_URL}`
 
