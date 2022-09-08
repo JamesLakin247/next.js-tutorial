655dfcb9bffb1ce32f1a2da6e10bebe186ce9c48
James <77265615+JamesLakin247@users.noreply.github.com>
2022-09-08 11:57:21 +1000

    change webdite url

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : "https://yourwebsite.com"
+export const server = dev ? 'http://localhost:3000' : "https://jameslakin247/next-tutorial-vercel"
 
