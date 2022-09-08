055172dbefc7303fe5e112807d96b1ff92e99b3b
James <77265615+JamesLakin247@users.noreply.github.com>
2022-09-08 12:53:46 +1000

    vercel url

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : "./"
+export const server = dev ? 'http://localhost:3000' : "https://next-js-tutorial-lhzt9k87s-jameslakin247.vercel.app/"
 
